918d9e387b1e5940caf1480ec858f7faca4d56e6
fabricioFalcao <fabriciohfd@gmail.com>
2023-05-16 19:07:30 -0300

    Removendo elementos

---
+++ fundamentos/secao-4-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-trabalhando-com-elementos/removendo-elementos.js
@@ -0,0 +1,8 @@
+const pai = document.getElementById('where-are-you');
+
+console.log(document.querySelector('#where-are-you h2'));
+
+// pai.removeChild(document.querySelector('#where-are-you h2'));
+
+
+pai.removeChild(pai.firstElementChild)

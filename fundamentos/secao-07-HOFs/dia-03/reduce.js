a249e6642254010a7f0da277f2890593bf3ec4e5
fabricioFalcao <fabriciohfd@gmail.com>
2023-06-13 16:50:52 -0300

    Exercicios finalizados

--- fundamentos/secao-07-HOFs/dia-03/reduce.js
+++ fundamentos/secao-07-HOFs/dia-03/reduce.js
@@ -65,3 +65,34 @@ const countries = [
     area: 91
   }
 ];
+
+const getPopulation = () => countries.reduce((acc, curr) => acc + curr.population, 0);
+
+const getTotalArea = () => countries.reduce((acc, curr) => acc + curr.area, 0);
+
+const longestName = () => countries.reduce((acc, curr) => acc.name.length > curr.name.length ? acc : curr)
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+const setArray = names.join('').toLowerCase().split('');
+const countA = () => setArray.reduce((acc, curr) => curr === 'a' ? acc += 1 : acc, 0);
+
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const expectedResult = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+const studentAverage = () => students.map((student, index) => (
+  {
+    name: student,
+    average: grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length,
+  }
+));

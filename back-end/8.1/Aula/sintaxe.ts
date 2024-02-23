4e1e422c215b1e248a5b8be4c30d5802b018f2f7
fabricioFalcao <fabriciohfd@gmail.com>
2024-02-23 13:36:08 -0300

    Sintaxe de encapsulamento

--- back-end/8.1/Aula/sintaxe.ts
+++ back-end/8.1/Aula/sintaxe.ts
@@ -1,24 +1,42 @@
 class TV {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo?: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo?: string;
 
-  constructor(b: string, s: number, r: string, c: string[]) {
-    this.brand = b;
-    this.size = s;
-    this.resolution = r;
-    this.connections = c;
+  constructor(b: string, s: number, r: string, c: string[], cT: string) {
+    this._brand = b;
+    this._size = s;
+    this._resolution = r;
+    this._connections = c;
+    this._connectedTo = cT;
   }
 
   turnOn(): void {
     console.log(
-      `TV ${this.brand}, ${this.size}", resolution: ${this.resolution}, available connections: ${this.connections}`
+      `TV ${this._brand}, ${this._size}", resolution: ${this._resolution}, available connections: ${this._connections}`
     );
 
   }
+
+  get connectedTo(): string | undefined {
+    return this._connectedTo;
+  }
+
+  set connectedTo(value: string | undefined) {
+    // permite setar undefined ou uma conexão que esteja no `connections`
+    if (!value || this._connections.includes(value)) {
+      this._connectedTo = value;
+      console.log(this._connectedTo);
+    } else {
+      console.log('Sorry, connection unavailable!');
+    }
+  }
 }
 
-const tv1 = new TV('Samsung', 50, '1080p', ['HDMI', 'USB', 'Ethernet', 'AVI',])
+const tv1 = new TV('Samsung', 50, '1080p', ['HDMI', 'USB', 'Ethernet', 'AVI',], 'HDMI')
+
 tv1.turnOn()
+tv1.connectedTo = 'AVI';
+console.log('Connected to: ', tv1.connectedTo);

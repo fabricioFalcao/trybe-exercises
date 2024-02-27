e172352e83953ebfbe168edd4b6f9e83c0aadfff
fabricioFalcao <fabriciohfd@gmail.com>
2024-02-27 15:30:38 -0300

    Sintaxe

---
+++ back-end/8.2/Aula/sintaxe.ts
@@ -0,0 +1,33 @@
+
+class Superclass {
+  isSuper: boolean;
+
+  constructor() {
+    this.isSuper = true;
+  }
+
+  public sayHello(): void {
+    console.log('Olá mundo!');
+  }
+}
+
+class Subclass extends Superclass {
+  constructor() {
+    super();
+    this.isSuper = false;
+  }
+}
+
+const myFunc = (object: Superclass) => {
+  object.sayHello();
+  console.log(object.isSuper ? 'Super!' : 'Sub!');
+};
+
+const sup = new Superclass();
+const sub = new Subclass();
+
+myFunc(sup);
+myFunc(sub);
+
+console.log(sup.isSuper);
+console.log(sub.isSuper);
